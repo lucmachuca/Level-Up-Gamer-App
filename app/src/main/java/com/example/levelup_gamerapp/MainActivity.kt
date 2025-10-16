0ba2fdb4a8da90070df2f88df7ee6a42d7a4ac78
Rodrigo <rojasrodrigo95@gmail.com>
2025-10-16 14:54:44 -0300

    Pantalla principal home, 1 version nada clickeable

--- app/src/main/java/com/example/levelup_gamerapp/MainActivity.kt
+++ app/src/main/java/com/example/levelup_gamerapp/MainActivity.kt
@@ -3,45 +3,13 @@ package com.example.levelup_gamerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.levelup_gamerapp.ui.theme.LevelUp_GamerAppTheme
+import com.example.levelup_gamerapp.PantallaPrincipal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            LevelUp_GamerAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            PantallaPrincipal()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LevelUp_GamerAppTheme {
-        Greeting("Android")
     }
 }
