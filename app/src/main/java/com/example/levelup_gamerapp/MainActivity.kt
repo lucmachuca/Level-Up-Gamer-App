1f196d1a4020c2fd403d727813232683c8c8f5cc
Rodrigo <rojasrodrigo95@gmail.com>
2025-10-18 17:31:07 -0300

    Navhost implementado nuevamente pensando a futuro, falta reimplementar drawer

--- app/src/main/java/com/example/levelup_gamerapp/MainActivity.kt
+++ app/src/main/java/com/example/levelup_gamerapp/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.levelup_gamerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.levelup_gamerapp.ui.NavHost
+import com.example.levelup_gamerapp.ui.AppNavHost
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavHost()
+            AppNavHost()
         }
     }
 }
